0bfa3f9ca561e64820b1a84660470d15fccb9171
NeoGuRi95 <jea5158@gmail.com>
2022-07-17 10:46:33 +0900

    백준 2460번

---
+++ src/bj2460.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class bj2460 {
+
+    static int[][] matrix = new int[10][2];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        for (int i = 0; i < 10; i++) {
+            st = new StringTokenizer(br.readLine());
+            matrix[i][0] = Integer.parseInt(st.nextToken());
+            matrix[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int maxPassenger = 0;
+        int nowPassenger = 0;
+
+        for (int i = 0; i < 10; i++) {
+            nowPassenger = nowPassenger - matrix[i][0] + matrix[i][1];
+            if (nowPassenger > maxPassenger) {
+                maxPassenger = nowPassenger;
+            }
+        }
+
+        System.out.println(maxPassenger);
+    }
+}
