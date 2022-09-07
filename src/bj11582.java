146934fb3167266440dff109fe20ab7bf25c6c01
NeoGuRi95 <jea5158@gmail.com>
2022-09-07 15:44:00 +0900

    백준 11582

---
+++ src/bj11582.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class bj11582 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine().trim());
+        List<Long> points = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            points.add(Long.parseLong(st.nextToken()));
+        }
+        int memberNum = Integer.parseInt(br.readLine().trim());
+
+        int subN = n / memberNum;
+
+        for (int i = 0; i < memberNum; i++) {
+            int start = i * subN;
+            int end = start + subN;
+            Collections.sort(points.subList(start,end));
+        }
+
+        List<String> strList = new ArrayList<>();
+        for (long p : points) {
+            strList.add(String.valueOf(p));
+        }
+        final String join1 = String.join(" ", strList);
+        System.out.println(join1);
+    }
+}
